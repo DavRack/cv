4f2ffef2ee603c5610be0907b94057adf10bdeb1
david_londono <David.Londono@nordstrom.com>
2025-05-29 12:35:34 -0500

    update

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,4 +2,4 @@
 
 # expects a name as a parameter that name should be the yaml file
 
-typst compile --input cvdata="$1.yaml" cv.typ "$1_cv.pdf"
+typst compile --input cvdata="data/$1.yaml" cv.typ "$1_cv.pdf"
